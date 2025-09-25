4f06b49a182974437838e47e016d38e60b61bf5d
Dufitimana Olivier <dufitimanaolivier64@gmail.com>
2025-09-25 12:02:31 +0200

    Add files via upload

---
+++ EXERCISES III/CODE/project14.py
@@ -0,0 +1,49 @@
+stack=[]
+#parctical ur student
+stack.append("open portal")
+stack.append("check grades")
+stack.append("Download Transcript")
+
+stack.pop()
+print(stack)
+
+
+
+#parctical momo pay
+stack=[]
+stack.append("select merchant")
+stack.append("enter amount")
+stack.append("confirm payment")
+
+stack.pop()
+stack.pop()
+print(stack)
+
+#challenge on stack
+
+
+
+queue=[]
+#parctical in chuk pharmacy
+queue.append("patient1")
+queue.append("patient2")
+queue.append("patient3")
+queue.append("patient4")
+queue.append("patient5")
+queue.append("patient6")
+queue.append("patient7")
+queue.append("patient8")
+queue.pop(0)
+queue.pop(0)
+queue.pop(0)
+queue.pop(0)
+print("queue after 4 patient is served:",queue[0])
+
+#parctical at airtel
+queue=[]
+queue.append("client1")
+queue.append("client2")
+queue.append("client3")
+queue.append("client4")
+queue.append("client5")
+print("the client who will be served first:",queue[0])
